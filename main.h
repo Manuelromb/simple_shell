76e5f777d6ba1c0f0dfbe734431b07847aa78631
Manuel Rombe Fungate Junior <manuelfungatejr@gmail.com>
2023-12-10 09:28:32 -0800

    first headers

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_SIZE 1024
+
+void display_prompt();
+#endif /*MAIN_H*/
